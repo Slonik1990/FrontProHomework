9ed43423252579f05487f2cd1532718361e915ec
Максим <starlock2015@gmail.com>
2022-07-10 23:10:05 +0300

    added homework 1

---
+++ hw1/script.js
@@ -0,0 +1,13 @@
+
+R = 10;
+H = 20;
+areaOfCircle = Math.PI * Math.pow(R, 2);
+volumeOfCylinder = areaOfCircle * H;
+
+console.log("**************");
+console.log(`Обьем цилиндра с площадью основы S = ${areaOfCircle}, радиусом R = ${R} и высотой H = ${H} равен:`);
+console.log("----------------------");
+console.log(`V = ${volumeOfCylinder}`);
+console.log("----------------------");
+console.log("end.");
+
