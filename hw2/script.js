d7475d52acf7081750730ce5bf24e4361dbf9735
Максим <starlock2015@gmail.com>
2022-07-10 23:10:17 +0300

    added homework 2

---
+++ hw2/script.js
@@ -0,0 +1,22 @@
+firstName = prompt("Please input your firstname").trim();
+lastName = prompt("Please input your lastname").trim();
+email = prompt("Please input your email").trim();
+email = email.toLowerCase();
+yearOFBirth = prompt("Please input your year of birth").trim();
+currentYear = new Date().getFullYear();
+age = currentYear - yearOFBirth;
+
+if (email.indexOf("@") === -1) {
+    email = `not valid email <b>${email}</b> (symbol @ not exist)`;
+} else if (email.indexOf("@") === 0) {
+    email = `not valid email <b>${email}</b> (symbol @ find in first place)`;
+} else if (email.indexOf("@") === email.length - 1) {
+    email = `not valid email <b>${email}</b> (symbol @ find in last place)`;
+}
+document.write(`
+<ul>
+    <li>Full name: ${firstName} ${lastName}</li>
+    <li>Email: ${email}</li>
+    <li>Age: ${age}</li>
+</ul>        
+`);
