ed392d180844527ddda164068d13b80f8b51012f
Максим <starlock2015@gmail.com>
2022-07-22 23:58:40 +0300

    added homework 9

--- hw9/script.js
+++ hw9/script.js
@@ -1,6 +1,3 @@
-
-//Вариант 1 с постепенным заполнением вспомогательного массива
-
 cardSuits = ["clubs", "diamonds", "hearts", "spades"];
 cardValue = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "T"];
 
@@ -31,33 +28,8 @@ for (i = 0; i < cardValue.length; i++) {
 document.write(`<div class="wrapper">${allCards.join("")}</div>`);
 
 
-//Вариант 2 с несколькими document.write
 
-cardSuits = ["clubs", "diamonds", "hearts", "spades"];
-cardValue = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "T"];
 
-document.write(`<div class="wrapper">`);
 
-for (i = 0; i < cardValue.length; i++) {
-    for (j = 0; j < cardSuits.length; j++) {
-        if (cardValue[i] === "J") {
-            middleOfCard = `<img class="person" src="images/jack.svg" alt="jack">`;
-        } else if (cardValue[i] === "Q") {
-            middleOfCard = `<img class="person" src="images/queen.svg" alt="queen">`;
-        } else if (cardValue[i] === "K") {
-            middleOfCard = `<img class="person" src="images/king.svg" alt="king">`;
-        } else if (cardValue[i] === "T") {
-            middleOfCard = `<img class="person" src="images/${cardSuits[j]}.svg" alt="${cardSuits[j]}">`;
-        } else {
-            middleOfCard = "";
-        }
-        document.write(`<div class="card">
-            <div class="info">${cardValue[i]}<img src="images/${cardSuits[j]}.svg" alt=${cardSuits[j]}></div>
-             ${middleOfCard}
-            <div class="info">${cardValue[i]}<img src="images/${cardSuits[j]}.svg" alt=${cardSuits[j]}></div>
-        </div>`);
-    }
-}
-document.write(`</div>`);
 
 
