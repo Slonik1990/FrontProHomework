fbeb555fc9b318377095ee6b577f67c141dfc1ea
Максим <starlock2015@gmail.com>
2022-07-26 00:57:06 +0300

    added homework 12

--- hw09/script.js
+++ hw09/script.js
@@ -1,4 +1,4 @@
-cardSuits = ["clubs", "diamonds", "hearts", "spades"];
+cardSuits = ["clubs", "diamonds", "spades"];
 cardValue = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "T"];
 
 allCards = [];
