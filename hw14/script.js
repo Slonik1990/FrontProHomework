19e2634070c46b71d9d4e28cd0527bcbbe4d4b98
Максим <starlock2015@gmail.com>
2022-07-29 17:04:45 +0300

    added homework 14

--- hw14/script.js
+++ hw14/script.js
@@ -1,174 +1,47 @@
+const obj = {
+    x: 10,
+    y: 20,
+    inner: {
+        x: 20,
+        z: 30,
+    },
+    foo2: {
+        k: 23, p: 13,
+    }
+}
 
-
-// Object, delete user.age
-
-// const user = {
-//   name: "Sasha",
-//   lastName: "Dontsova",
-//   age: 26,
-//   isCat: true,
-//   nameOfCat: ["Siri", "Mars"],
-//   husband: {
-//     name: "Illia",
-//     lastName: "Dontsov",
-//     age: 27,
-//     parents: {
-//       mother: {
-//         name: "abcde",
-//         age: 40,
-//       },
-//       father: {
-//         name: "zxcvb",
-//         age: 50,
-//       }
+// //Вариант 1 без рекурсии, сработает при одном уровне вложенности
+// //Задачу ДЗ выполняет
+// function convert(object) {
+//     let newObj = {};
+//     for(let key in object) {
+//         if(typeof object[key] === 'object') {
+//             for(let innerKey in object[key]) {
+//                 newObj[innerKey] = object[key][innerKey]
+//             }
+//         } else {
+//             newObj[key] = object[key];
+//         }
 //     }
-//   }
+//     return newObj;
 // };
 
-// console.log(user.name);
-// console.log(user.lastName);
-// console.log(user.nameOfCat[1]);
-// console.log(user.husband.parents.mother.age);
 
-// let users = [
-//   {
-//     name: "Artem",
-//     age: 25,
-//     email: "dffgh@dd.com",
-//   },
-//   {
-//     name: "Anton",
-//     age: 27,
-//     email: "kjewf23@dd.com",
-//   },
-//   {
-//     name: "Anna",
-//     age: 20,
-//     email: "jkewf000@dd.com",
-//   },
-//   {
-//     name: "Kristina",
-//     age: 23,
-//     email: "dsjkwfekjbw122@dd.com",
-//   },
-// ];
-
-// users.forEach(
-//   function(item) {
-//     if(item.age > 23) {
-//       document.write(`
-//         <p>Имя: ${item.name} - врозслый</p>
-//       `)
-//     } else {
-//       document.write(`
-//         <p>Имя: ${item.name} - подросток</p>
-//       `)
-//     }
-//   }
-// )
+//Вариант 2 с рекурсией, работате с любым колличеством уровней вложенности
+function convert(object){
+    let result = {};
+    recordIterator(object, result);
+    return result;
+}
+function recordIterator(object, currentState) {
+    for(let key in object) {
+        if(typeof object[key] === 'object') {
+            recordIterator(object[key],currentState);
+        } else {
+            currentState[key] = object[key];
+        }
+    }
+}
 
 
-// let animal = {
-//   type: "Lion",
-//   age: 8,
-//   color: "white"
-// };
-
-// animal.name = "Mufasa";
-// animal.teritory = "Africa";
-
-// let key = "africa country";
-// animal[key] = "Tanzaniia";
-
-// delete animal.age;
-// delete animal.name;
-
-// console.log(animal);
-
-// const animal = {
-//     type: "Lion",
-//     age: 8,
-//     color: "white",
-//     food: {
-//         meat: ["Antilopa", "Kozel"],
-//         vegatables: "Cabbage",
-//     }
-// };
-//
-// animal.name = "Mufasa";
-//
-// console.log(animal);
-// // debugger;
-// // for(let key in animal) {
-// //   if(key === "age") {
-// //     console.log(`Humane age - ${animal[age] * 4}`);
-// //   }
-// // }
-//
-//
-// const arr = [1, 23, 78, 123];
-//
-// console.log(arr);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let user = {
-//   __proto__: {
-//     constructor: ƒ Object()
-//     hasOwnProperty: ƒ hasOwnProperty()
-//     isPrototypeOf: ƒ isPrototypeOf()
-//     propertyIsEnumerable: ƒ propertyIsEnumerable()
-//     toLocaleString: ƒ toLocaleString()
-//     toString: ƒ toString()
-//     valueOf: ƒ valueOf()
-//     __defineGetter__: ƒ __defineGetter__()
-//     __defineSetter__: ƒ __defineSetter__()
-//     __lookupGetter__: ƒ __lookupGetter__()
-//     __lookupSetter__: ƒ __lookupSetter__()
-//     __proto__: (...)
-//     get __proto__: ƒ __proto__()
-//     set __proto__: ƒ __proto__()
-//   }
-// }
-
-// let arr = [
-
-//   __proto__: {
-//     map: function(){},
-//     push: function(){},
-//     __proto__: {
-//       constructor: ƒ Object()
-//       hasOwnProperty: ƒ hasOwnProperty()
-//       isPrototypeOf: ƒ isPrototypeOf()
-//       propertyIsEnumerable: ƒ propertyIsEnumerable()
-//       toLocaleString: ƒ toLocaleString()
-//       toString: ƒ toString()
-//       valueOf: ƒ valueOf()
-//       __defineGetter__: ƒ __defineGetter__()
-//       __defineSetter__: ƒ __defineSetter__()
-//       __lookupGetter__: ƒ __lookupGetter__()
-//       __lookupSetter__: ƒ __lookupSetter__()
-//       __proto__: (...)
-//       get __proto__: ƒ __proto__()
-//       set __proto__: ƒ __proto__()
-//     }
-//   }
-// ]
+console.log(convert(obj))
