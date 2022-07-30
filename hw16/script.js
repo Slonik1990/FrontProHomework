620dc1cbecf120cee060d921250aa178045b7fdc
Максим <starlock2015@gmail.com>
2022-07-30 20:21:13 +0300

    added homework 16

--- hw16/script.js
+++ hw16/script.js
@@ -24,23 +24,21 @@ let dcHeroes = [
 ];
 
 Array.prototype.heroesRender = function (directory) {
-    let tableRows = [];
-    for (let i = 0; i < this.length; i++) {
-        let imageName = this[i].name.toLowerCase().replaceAll(" ", "");
-        tableRows.push(`
-            <tr>
-                 <td>${this[i].name}</td>
-                 <td><img src="images/${directory}/${imageName}.svg" alt="${this[i].name}"></td>
-            </tr>
-        `)
-    }
-    tableRows = `<tbody>${tableRows.join()}</tbody>`;
+
+    let tableRows = this.map(
+        function (item) {
+            let imageName = item.name.toLowerCase().replaceAll(" ", "");
+            return `<tr>
+                         <td>${item.name}</td>
+                         <td><img src="images/${directory}/${imageName}.svg" alt="${item.name}"></td>
+                    </tr>`
+        }
+    ).join("");
+
+    tableRows = `<tbody>${tableRows}</tbody>`;
     let tableHeader = `<thead><th>Name</th><th>Icon</th></thead>`
 
-    document.write(`<table>
-                    ${tableHeader};
-                    ${tableRows};
-                </table>`);
+    document.write(`<table>${tableHeader}${tableRows}</table>`);
 }
 
 dcHeroes.heroesRender("dc")
