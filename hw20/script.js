f82c5e9c0899dab1fcc561c9f0029dc283ccf0f1
Максим <starlock2015@gmail.com>
2022-08-25 09:59:38 +0300

    added homework 20

---
+++ hw20/script.js
@@ -0,0 +1,52 @@
+const root = document.querySelector(".root");
+
+
+function firstLastChildDecorator(startElement, level){
+    const parentElements = getParentElementsArray(startElement, level);
+    parentElements.forEach((parent) => addingClassesToFirstLastChild(parent));
+}
+
+//функция которая рекурсивно вернет массив родительских элементов
+//с детишками которых будут происходить изменения
+function getParentElementsArray(start, level){
+    const parents = [];
+    if (level === 1) {
+        parents.push(start);
+    } else {
+        [...start.children].forEach(function (item){
+            parents.push(...getParentElementsArray(item, level - 1));
+        });
+    }
+    return parents;
+}
+//добавит классы первому и последнему элементу принимаемого родителя
+function addingClassesToFirstLastChild(parentElement){
+    parentElement.firstElementChild.classList.add("first");
+    parentElement.lastElementChild.classList.add("last");
+}
+
+firstLastChildDecorator(root,2);
+
+
+//развлечения
+function redGreenCleaner(current){
+    if (current.children.length) {
+        current.classList.remove("first");
+        current.classList.remove("last");
+        [...current.children].forEach(function (child){
+            redGreenCleaner(child);
+        });
+    } else {
+        current.classList.remove("first");
+        current.classList.remove("last");
+    }
+}
+root.onclick = function (){
+    redGreenCleaner(root);
+    const randomLevel = Math.floor(Math.random()*3+1);
+    firstLastChildDecorator(root, randomLevel);
+}
+
+
+
+
---
+++ zametki/prototpe/randomColor.js
@@ -0,0 +1,14 @@
+// function randomNumber(){
+//     const min = 0;
+//     const max = 255;
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+// function randomColor(){
+//     let r = randomNumber();
+//     let g = randomNumber();
+//     let b = randomNumber();
+//     return `rgb(${r}, ${g}, ${b})`;
+// }
+// setInterval(() => {
+//     outer.style.backgroundColor = randomColor();
+// }, 1000);
